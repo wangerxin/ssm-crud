815ad223183412c6ab74f7c0030dad369f806b28
wangerxin <997520707@qq.com>
2020-02-10 11:33:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/crud/test/MBGTest.java
@@ -0,0 +1,28 @@
+package com.atguigu.crud.test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ * 逆向工程： 根据mysql表生成javabean和mapper（新手不建议使用逆向工程）
+ */
+public class MBGTest {
+
+	public static void main(String[] args) throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("mbg.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+}
